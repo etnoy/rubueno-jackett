8c4dacca980d5e39e08dec79dd222df95a12eea6
Jonathan Jogenfors <etnoy@broach.se>
2019-08-10 01:14:44 +0200

    Correct if switch logic

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,15 @@
 class jackett::params {
 
-  $additional_packages = ['curl',
-    'Ubuntu'  => 'libcurl4-openssl-dev',
-    'Debian'  => 'libcurl4-openssl-dev',
-    'CentOS'  => 'libcurl-devel',
-    'RHEL'    => 'libcurl-devel',
-    'Fedora'  => 'libcurl-devel',
-    default => 'libcurl_dev',
+  $additional_packages = [
+    'curl',
+    $::operatingsystem ? {
+      'Ubuntu'  => 'libcurl4-openssl-dev',
+      'Debian'  => 'libcurl4-openssl-dev',
+      'CentOS'  => 'libcurl-devel',
+      'RHEL'    => 'libcurl-devel',
+      'Fedora'  => 'libcurl-devel',
+      default => 'libcurl_dev',
+    }
   ]
   $user                = 'jackett'
   $base_path           = '/opt/Jackett'
